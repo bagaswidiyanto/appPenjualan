53fe17329e2e0e6d93acc647301cdbae506cbe13
bagaswidiyanto <bagaswidiyanto1@gmail.com>
2020-05-17 22:13:09 +0700

    Hello

--- app/src/main/java/com/example/aplikasi_penjualan/MainActivity.java
+++ app/src/main/java/com/example/aplikasi_penjualan/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.aplikasi_penjualan;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -14,17 +16,13 @@ public class MainActivity extends AppCompatActivity {
     private EditText editnamapel, editnamabar,
             editjumlahbar, editharga, edituangbay;
     private Button btnproses;
-    private Button btnhapus;
+    private Button btnreset;
     private Button btnexit;
     private TextView txtnamapel;
     private TextView txtnamabar;
     private TextView txtjumlahbar;
     private TextView txtharga;
     private TextView txtuangbay;
-    private TextView txtbonus;
-    private TextView txttotalbelanja;
-    private TextView txtkembali;
-    private TextView txtketerangan;
 
 
     @Override
@@ -40,7 +38,7 @@ public class MainActivity extends AppCompatActivity {
         edituangbay   = (EditText)findViewById(R.id.jml_uang);
 
         btnproses     = (Button)findViewById(R.id.proses);
-        btnhapus      = (Button)findViewById(R.id.reset);
+        btnreset      = (Button)findViewById(R.id.reset);
         btnexit       = (Button)findViewById(R.id.exit);
 
         txtnamapel    = (TextView)findViewById(R.id.nama_pelanggan);
@@ -50,14 +48,15 @@ public class MainActivity extends AppCompatActivity {
         txtuangbay    = (TextView)findViewById(R.id.jml_uang);
 
         //setelah prosses
-        txttotalbelanja = (TextView)findViewById(R.id.total);
-        txtkembali      = (TextView)findViewById(R.id.kembalian);
-        txtketerangan   = (TextView)findViewById(R.id.keterangan);
-        txtbonus        = (TextView)findViewById(R.id.bonus);
+//        txttotalbelanja = (TextView)findViewById(R.id.total);
+//        txtkembali      = (TextView)findViewById(R.id.kembalian);
+//        txtketerangan   = (TextView)findViewById(R.id.keterangan);
+//        txtbonus        = (TextView)findViewById(R.id.bonus);
 
         btnproses.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
 
                 String namapelanggan = editnamapel.getText().toString().trim();
                 String namabarang    = editnamabar.getText().toString().trim();
@@ -65,40 +64,63 @@ public class MainActivity extends AppCompatActivity {
                 String hargabarang   = editharga.getText().toString().trim();
                 String uangbayar     = edituangbay.getText().toString().trim();
 
-                double jb    = Double.parseDouble(jumlahbarang);
-                double h     = Double.parseDouble(hargabarang);
-                double ub    = Double.parseDouble(uangbayar);
-                double total = (jb*h);
+                if(namapelanggan.equals("") || namabarang.equals("") || jumlahbarang.equals("") || hargabarang.equals("") || uangbayar.equals("")) {
+                    Toast.makeText(getApplicationContext(), "Mohon Lengkapi Data", Toast.LENGTH_SHORT).show();
+                } else {
+                    double jb    = Double.parseDouble(jumlahbarang);
+                    double h     = Double.parseDouble(hargabarang);
+                    double ub    = Double.parseDouble(uangbayar);
+                    double total = (jb*h);
+
+                    Bundle b = new Bundle();
+                    b.putString("nama_pelanggan", "Nama Pelanggan :" + namapelanggan);
+                    b.putString("nama_barang", "Nama Barang : " + namabarang);
+                    b.putString("jumlah_barang","Jumlah Barang : " + jumlahbarang);
+                    b.putString("harga_barang", "Harga Barang : "+hargabarang);
+                    b.putString("jumlah_uang", "Uang Bayar : "+uangbayar);
+                    b.putString("total_belanja", "Total Belanja : "+total);
 
 
-                txttotalbelanja.setText("Total Belanja : "+total);
+                    if (total >= 2000000){
+                        b.putString("bonus", "Bonus : Hardisk");
+                    }
+                    else if (total >= 1500000){
+                        b.putString("bonus", "Bonus : Keyboard");
+                    }
+                    else if (total >= 1000000){
+                        b.putString("bonus", "Bonus : Mouse");
+                    }
+                    else if (total >= 500000){
+                        b.putString("bonus", "bonus : Flash Disk");
+                    }
+                    else {
+                        b.putString("bonus", "Bonus : Tidak Ada Bonus");
+                    }
 
-                if (total >= 200000){
-                    txtbonus.setText("Bonus : Hardisk");
-                }
-                else if (total >= 50000){
-                    txtbonus.setText("Bonus : Keyboard");
-                }
-                else if (total >= 40000){
-                    txtbonus.setText("Bonus : Mouse");
-                }
-                else {
-                    txtbonus.setText("Bonus : Tidak Ada Bonus");
+
+                    double uangkembalian = (ub-total);
+                    if (ub<total){
+                        b.putString("keterangan", "Keterangan : uang bayar kurang Rp. "+(-uangkembalian));
+                        b.putString("kembalian", "Uang Kembalian : Rp. 0");
+                    }
+                    else {
+                        b.putString("keterangan", "Keterangan : Tunggu Kembalian ");
+                        b.putString("kembalian", "Uang Kembalian : "+uangkembalian);
+                    }
+
+                    Intent intent = new Intent( v.getContext(), HasilPenjualan.class);
+                    intent.putExtras(b);
+                   v.getContext().startActivity(intent);
                 }
 
 
-                double uangkembalian = (ub-total);
-                if (ub<total){
-                    txtketerangan.setText("Keterangan : uang bayar kurang Rp. "+(-uangkembalian));
-                    txtkembali.setText("Uang Kembalian : Rp. 0");
-                }
-                else {
-                    txtketerangan.setText("Keterangan : Tunggu Kembalian");
-                    txtkembali.setText("Uang Kembalian : "+uangkembalian);
-                }
+
+
+
+
             }
         });
-        btnhapus.setOnClickListener(new View.OnClickListener() {
+        btnreset.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 txtnamapel.setText(" ");
@@ -106,12 +128,8 @@ public class MainActivity extends AppCompatActivity {
                 txtjumlahbar.setText(" ");
                 txtharga.setText(" ");
                 txtuangbay.setText(" ");
-                txtkembali.setText("Uang Kembali : Rp. 0");
-                txtketerangan.setText(" ");
-                txtbonus.setText(" ");
-                txttotalbelanja.setText("Total Belanja : Rp.0");
 
-                Toast.makeText(getApplicationContext(), "Data sudah dihapus", Toast.LENGTH_LONG).show();
+                Toast.makeText(getApplicationContext(), "Data sudah direset", Toast.LENGTH_LONG).show();
             }
         });
         btnexit.setOnClickListener(new View.OnClickListener() {
